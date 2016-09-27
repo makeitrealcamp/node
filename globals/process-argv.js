67acedc16aa2437e8184e01cb3930f0d7eba3b0f
German Hernandez <muygrafico@gmail.com>
2016-09-27 15:00:47 -0500

    add argv

--- globals/process-argv.js
+++ globals/process-argv.js
@@ -1,1 +1,33 @@
-// Tu codigo va aca
+
+/*var findUser = process.argv.indexOf('--user') === -1;
+var findGreeting = process.argv.indexOf('--greeting') === -1;
+console.log(findUser,findGreeting)
+
+
+if ( findUser || findGreeting ){
+  console.log('Algo salio mal')
+}else{
+  var greetingIndex = process.argv.indexOf('--greeting') + 1;
+  var greeting = process.argv[greetingIndex];
+
+  var userIndex = process.argv.indexOf('--user') + 1;
+  var user = process.argv[userIndex];
+
+  console.log(`Hola ${user}, ${greeting}`)
+}*/
+
+var findUser = process.argv.indexOf('--user')=== -1;
+var findGreeting = process.argv.indexOf('--greeting'=== -1);
+console.log(findUser,findGreeting)
+
+if(findUser || findGreeting ){
+  var greetingIndex = process.argv.indexOf('--greeting') + 1;
+  var greeting = process.argv[greetingIndex];
+
+  var userIndex = process.argv.indexOf('--user') + 1;
+  var user = process.argv[userIndex];
+
+  console.log(`Hola ${user}, ${greeting}`)
+}else {
+  console.log('Algo salio mal')
+}
