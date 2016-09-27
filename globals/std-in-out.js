edd1f10616172eec48874de7edd8782f479a3a1f
German Hernandez <muygrafico@gmail.com>
2016-09-27 16:03:14 -0500

    bla

--- globals/std-in-out.js
+++ globals/std-in-out.js
@@ -5,3 +5,32 @@ var questions = [
   "What is your fav hobby?",
   "What is your preferred programming language?"
 ];
+
+
+
+
+//var wName = process.stdout.write(questions[0]);
+
+var index = 0;
+function askQuestion(questionsArr){
+  process.stdout.write(questionsArr[index])
+  if (index < 2){ index+=1;}
+  console.log(index)
+  if (index === 2){
+    console.log('jajaja')
+  }
+
+}
+
+askQuestion(questions);
+
+process.stdin.on('data', function(data){
+  askQuestion(questions);
+})
+  process.exit()
+
+
+  // var nameAnswer = data.toString().trim()
+  //  process.stdout.write(questions[1])
+  // console.log(nameAnswer);
+  //  process.exit()
