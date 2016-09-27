ade3716a9bd0e30f3b9ffdadb970df2fe4353830
juan267 <j.gomez267@gmail.com>
2016-09-27 13:06:57 -0500

    first commit

---
+++ fileSystem/readdir.js
@@ -0,0 +1,13 @@
+// Necesitamos estos dos modulos del core de Node para poder Completar este reto
+
+var fs = require('fs')
+var path = require('path')
+
+// Version Syncronica
+
+
+// Version asyncrona
+
+
+
+console.log('Suspenso no se donde voy a salir')
