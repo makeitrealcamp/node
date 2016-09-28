c941a10126314ad63c5c095d8d53166c69d98909
German Hernandez <muygrafico@gmail.com>
2016-09-28 09:05:55 -0500

    step 1 - fileSystem

--- fileSystem/readdir.js
+++ fileSystem/readdir.js
@@ -2,12 +2,20 @@
 
 var fs = require('fs')
 var path = require('path')
+var dirpath =  path.join( path.dirname(__filename) , 'testFiles')
 
 // Version Syncronica
 
+// var files = fs.readdirSync(dirpath)
+// console.log('sync: ',files)
 
 // Version asyncrona
 
-
+fs.readdir( dirpath , 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Async:', data);
+});
 
 console.log('Suspenso no se donde voy a salir')
