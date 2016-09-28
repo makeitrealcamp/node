a534fbb93f606205ca615976bcdf131da1eca30f
German Hernandez <muygrafico@gmail.com>
2016-09-28 09:42:58 -0500

    fileSystem step 2

--- fileSystem/readFile.js
+++ fileSystem/readFile.js
@@ -1,3 +1,33 @@
 var path = require('path')
 var fs = require('fs')
+var dirpath =  path.join( path.dirname(__filename) , 'testFiles')
 
+
+function checkIfDerectory(pathIn){
+  return fs.lstatSync(pathIn).isDirectory()
+}
+
+function bufferToString(data){
+  return data.toString().trim()
+}
+
+fs.readdir( dirpath , 'utf8', function (err,files) {
+  if (err) {
+    return console.log(err)
+  }
+
+  for (var i in files) {
+
+    var pathIn = path.join( dirpath , path.basename(files[i]) )
+    if ( !checkIfDerectory(pathIn) ){
+
+      fs.readFile(pathIn, (err, data) => {
+        if (err) throw err;
+        console.log('\n')
+        console.log( bufferToString(data) );
+      })
+
+    }
+
+  }
+});
